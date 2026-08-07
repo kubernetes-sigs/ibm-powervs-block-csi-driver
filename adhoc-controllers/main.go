3b286e75503986dad4fc5a469e0d2d2dd16a5485
Kishen Viswanathan <kishen.viswanathan@ibm.com>
2026-08-07 11:40:26 +0530

    Authenticate and authorize node-update-controller metrics endpoint
    Enable SecureServing and WithAuthenticationAndAuthorization on the
    controller-runtime metrics.

--- adhoc-controllers/main.go
+++ adhoc-controllers/main.go
@@ -27,6 +27,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/ibm-powervs-block-csi-driver/adhoc-controllers/controllers"
@@ -71,8 +72,12 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		Metrics:                server.Options{BindAddress: metricsAddr},
+		Scheme: scheme,
+		Metrics: server.Options{
+			BindAddress:    metricsAddr,
+			SecureServing:  true,
+			FilterProvider: filters.WithAuthenticationAndAuthorization,
+		},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "15c66b50.my.domain",
